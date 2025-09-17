e9b9c8c163108dc9b90a3e3169b04371eab8b20f
LetsCodeIt-all <krishnay9699@gmail.com>
2025-09-17 21:59:25 +0530

    17Sep

--- src/Components/Delivery.jsx
+++ src/Components/Delivery.jsx
@@ -1,7 +1,35 @@
 import React from "react";
-
+import style from "../Scss/Checkout.module.scss";
 function Delivery() {
-  return <div></div>;
+  return (
+    <div className={style.Delivery}>
+      <div className={style.Address}>
+        <h1>Delivery Address</h1>
+        <div>
+          <p> Gala no. 1 waghral pada, Bhoida Pada, Sativali,Vasai E 401208</p>
+          <button>Change</button>
+        </div>
+      </div>
+      <div className={style.dateOfDelivery}>
+        <h2>Shipping Method</h2>
+        <form>
+          <div className={style.Divein}>
+            <input type="radio" name="" id="Standard" />
+
+            <label htmlFor="Standard">
+              <div className={style.innerBx}>
+                <div className={style.innerMost}>
+                  <h3>Standard Delivery</h3>
+                  <p> after 2 days delivery</p>
+                </div>
+                <h3>Free</h3>
+              </div>
+            </label>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Delivery;
--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
 import style from "../Scss/Checkout.module.scss";
 import ProgressBar from "../Components/ProgressBar";
+import Delivery from "../Components/Delivery";
 function Checkout() {
   const [progress, setProgress] = useState(0);
   return (
     <div className={style.Checkout}>
       <ProgressBar prop={progress} />
+      <Delivery />
       <button
         onClick={() => {
-          setProgress(progress + 50);
+          if (progress < 100) {
+            setProgress(progress + 50);
+          }
         }}
       >
         Next
