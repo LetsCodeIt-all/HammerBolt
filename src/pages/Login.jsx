a838579fe97edde6c89bbeaddfda452010a885fb
LetsCodeIt-all <krishnay9699@gmail.com>
2025-09-21 23:14:57 +0530

    21Sep

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 import "../Scss/Login.scss";
-import image from "../assets/Capture.png";
 function Login() {
   return (
     <div className="Login">
       <div>
-        <img src={image} alt="" />
-      </div>
-      <div>
         <h2>Welcome Back</h2>
         <p>Login to your account</p>
-        <input type="text" name="" id="" />
-        <input type="password" />
+        <input type="text" name="" id="" placeholder="Email" />
+        <input type="password" placeholder="password" />
         <p>Forgot Password?</p>
         <button>Login</button>
-        <button>Continue with google</button>
+        <button>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/1200px-Google_%22G%22_logo.svg.png"
+            alt=""
+          />
+          Continue with google
+        </button>
       </div>
     </div>
   );
